8ab96a44040888a82692de2c0157e43ed55ce96f
aayampokharel <aayam.pokharel@gmail.com>
2024-07-09 16:12:33 +0545

    changed getNamesFromUserSearchHistory.go and getnamesfromlogintable.go

--- getnNamesFromUserSearchHistory.go
+++ getnNamesFromUserSearchHistory.go
@@ -8,6 +8,7 @@ import (
 )
 
 func getNamesFromUserSearchHistoryTable(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	//@ this should be run when flag=true or 1 from client to get the history of names . 
 	var currentUserId UserId;
 	err:=json.NewDecoder(r.Body).Decode(&currentUserId);
 	if err!=nil{
@@ -21,13 +22,17 @@ inner join loginCredential_table l1 on s1.other_user_id=l1.registration_id where
 rows,er:=db.Query(query,currentUserId.UserId);
 if er!=nil{
 	fmt.Print("cant select a problem in db has arrived ");
-
+return; 
 }
 var temp string;
 var tempList []string;
 
 for rows.Next(){
-rows.Scan(&temp);
+er:=rows.Scan(&temp);
+if er!=nil{
+	fmt.Print("error while sdcanning from getNamesfromusersearchhistory.go");
+	return;
+}
 tempList = append(tempList, temp);
 }
 err=json.NewEncoder(w).Encode(tempList);
--- getnamesfromlogintable.go
+++ getnamesfromlogintable.go
@@ -8,21 +8,31 @@ import (
 )
 
 
-func getNamesFromLoginTable(db *sql.DB,w http.ResponseWriter,_ *http.Request) {
-	//@ gets names fromlogincredential_table should be used when flag=NO.
+func getNamesFromLoginTable(db *sql.DB,w http.ResponseWriter,r *http.Request) {
+	//@ gets names from logincredential_table should be used when flag=NO.
 	var displayName string;
+	var currentUserId UserId;
 	var historyOfNames []string;
 
-	 query:=`select display_name from loginCredential_table `;
+	err:=json.NewDecoder(r.Body).Decode(&currentUserId);
+	if err!=nil{
+		fmt.Print("error while decoding from getnamesfromlogintable.go")
+	}
 
-	 rows, err:= db.Query(query);
+	 query:=`select display_name from loginCredential_table where registration_id !=?`;//@ !=kina bhanda it just helps in things like getting all names except self as search ma afnai name search garna milena.
+
+	 rows, err:= db.Query(query,currentUserId.UserId);
 	 if err!=nil{
 		fmt.Print("error while selecting from getNames.go");
 	 }
 
 	 for rows.Next(){
 	
-		rows.Scan(&displayName);
+		er:=rows.Scan(&displayName);
+		if er!=nil{
+			fmt.Print("error while scanningg from getnamesfromlogintable.go")
+			return;
+		}
 historyOfNames=append(historyOfNames, displayName);
 	 }
 	er:= json.NewEncoder(w).Encode(historyOfNames);
